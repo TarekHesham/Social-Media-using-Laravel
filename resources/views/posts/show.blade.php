be48bd65bc8e233a3c129e2ad87841fb3f1e5ae0
Tarek Hesham <tarekghost6@gmail.com>
2024-10-24 21:12:45 +0300

    Edit Show Posts Page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,22 +11,22 @@ Show Post
 
     <div class="card-body">
         @if ($post->image)
-            <img src="{{asset("images/posts/{$post->image}")}}" width="250" height="250" class="mb-3 my-auto">
+        <img src="{{asset("images/posts/{$post->image}")}}" class="mb-3 mx-auto w-100">
         @endif
         <blockquote class="blockquote mb-0">
-        <h4><strong>Title:-</strong><br> {{$post->title}}</h4>
-        <p>
-            <strong>Description:-</strong><br>
-            {{$post->description}}
-        </p>
-        <br>
-        <footer class="blockquote-footer float-start">
-            Posted By: <cite>{{$post->creator->name}}</cite>
+            <h4><strong>Title:-</strong><br> {{$post->title}}</h4>
+            <p>
+                <strong>Description:-</strong><br>
+                {{$post->description}}
+            </p>
             <br>
-            Created At: {{ $post->created_at }}
-            <br>
-            Updated In {{ $post->updatedAt }}
-        </footer>
+            <footer class="blockquote-footer float-start">
+                Posted By: <cite>{{$post->creator->name}}</cite>
+                <br>
+                Created At: {{ $post->created_at }}
+                <br>
+                Updated In {{ $post->updatedAt }}
+            </footer>
         </blockquote>
     </div>
 </div>
@@ -35,14 +35,14 @@ Show Post
     <h3>Comments</h3>
     <div class="comments">
         @foreach ($post->comments as $comment)
-            <div class="comment mb-3 p-3 rounded shadow bg-light">
-                <div class="d-flex align-items-center">
-                    <img src="{{Str::startsWith($comment->creator->image, 'https') ? $comment->creator->image : asset("images/users/{$comment->creator->image}")}}" alt="Avatar" class="image" style="width: 25px; height: 25px; border-radius: 50%;">
-                    <h5 class="m-0 ms-1">{{$comment->creator->name}}</h5>
-                </div>
-                <p class="m-0 mt-1">{{$comment->content}}</p>
-                <small class="text-muted">{{$comment->created_at->diffForHumans()}}</small>
+        <div class="comment mb-3 p-3 rounded shadow bg-light">
+            <div class="d-flex align-items-center">
+                <img src="{{Str::startsWith($comment->creator->image, 'https') ? $comment->creator->image : asset("images/users/{$comment->creator->image}")}}" alt="Avatar" class="image" style="width: 25px; height: 25px; border-radius: 50%;">
+                <h5 class="m-0 ms-1">{{$comment->creator->name}}</h5>
             </div>
+            <p class="m-0 mt-1">{{$comment->content}}</p>
+            <small class="text-muted">{{$comment->created_at->diffForHumans()}}</small>
+        </div>
         @endforeach
     </div>
 
@@ -54,13 +54,13 @@ Show Post
             </div>
 
             @error('content')
-                <div class="alert alert-danger">{{ $message }}</div>
+            <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             <button type="submit" class="btn btn-primary float-end mb-5">Reply</button>
         </form>
     </div>
 </section>
-{{-- 
+{{--
 <script>
     const commentContent = document.getElementById('commentContent');
     commentContent.addEventListener('keyup', function(e) {
